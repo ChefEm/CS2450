311a658b78372950b1a9d67b05a058faaa9cc2e5
acvenegas <98815344+acvenegas@users.noreply.github.com>
2023-11-15 19:42:49 -0800

    Update Main.java

--- CS2450FinalProject/src/application/Main.java
+++ CS2450FinalProject/src/application/Main.java
@@ -5,6 +5,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
